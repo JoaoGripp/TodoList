ef81c8f500d1a55482e87268c13b1a733d2bee47
JoaoGripp <corguinha@gmail.com>
2021-10-26 12:10:19 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Joao Gripp on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
