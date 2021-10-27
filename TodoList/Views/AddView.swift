b957915872ca04773fa1fd30be4be10669e6cfb7
JoaoGripp <corguinha@gmail.com>
2021-10-27 09:56:11 -0300

    Added Alert to save item

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -13,6 +13,9 @@ struct AddView: View {
     @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
     
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -36,11 +39,27 @@ struct AddView: View {
             .padding(14)
         }
         .navigationTitle("Add an Item 🖋")
+        .alert(isPresented: $showAlert, content: getAlert)
     }
     
     func saveButtonPressed() {
-        listViewModel.addItem(title: textFieldText)
-        presentationMode.wrappedValue.dismiss()
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "You new tdo item must be at least 3 characters long!!!\n 😨😰😱"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
