dc7ecaf9048f2af91ab69eb80e4dedac0de2f94b
JoaoGripp <corguinha@gmail.com>
2021-10-27 13:53:59 -0300

    Finished Updating the project

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -22,7 +22,7 @@ struct AddView: View {
                 TextField("Type something here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 55)
-                    .background(Color(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
+                    .background(Color(UIColor.secondarySystemBackground))
                 .cornerRadius(10)
                 
                 Button (action: saveButtonPressed, label: {
@@ -65,9 +65,17 @@ struct AddView: View {
 
 struct AddView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            AddView()
+        Group {
+            NavigationView {
+                AddView()
+            }
+            .preferredColorScheme(.light)
+            .environmentObject(ListViewModel())
+            NavigationView {
+                AddView()
+            }
+            .preferredColorScheme(.dark)
+            .environmentObject(ListViewModel())
         }
-        .environmentObject(ListViewModel())
     }
 }
